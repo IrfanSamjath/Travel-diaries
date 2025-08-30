bf320128f5978c24f86399df99b42e948408afad
IrfanSamjath <irfansamjath17@gmail.com>
2025-08-31 00:43:26 +0530

    Update MONGODB_URI with correct password

--- backend/env-setup.js
+++ backend/env-setup.js
@@ -3,7 +3,7 @@ console.log('Environment variables loaded from:', __dirname + '/.env');
 
 // Force set MONGODB_URI if not set in .env
 if (!process.env.MONGODB_URI) {
-  process.env.MONGODB_URI = 'mongodb+srv://IrfanSamjath:Samfee%40876@cluster0.b8rroad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  process.env.MONGODB_URI = 'mongodb+srv://IrfanSamjath:samjath786@cluster0.b8rroad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
   console.log('MONGODB_URI was not set, defaulting to:', process.env.MONGODB_URI);
 }
 
