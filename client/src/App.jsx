7411f8eed454fcb766aef790871b24ef9499903e
IrfanSamjath <irfansamjath17@gmail.com>
2025-09-07 01:55:19 +0530

    Fix frontend routing and vite config for deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,19 @@
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
-import About from "./pages/About";
 import PostDetail from "./pages/PostDetail";
-import "./App.css";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/blog/:id" element={<PostDetail />} />
-        </Routes>
-      </main>
-    </>
+    <Router basename={import.meta.env.BASE_URL || "/"}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:id" element={<PostDetail />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
