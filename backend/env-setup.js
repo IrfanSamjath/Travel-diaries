7c59e0e338c50b91e3e4cd32b8a02cce5b30dc51
IrfanSamjath <irfansamjath17@gmail.com>
2025-09-07 01:28:17 +0530

    Update MongoDB URI with correct password for production deployment

--- backend/env-setup.js
+++ backend/env-setup.js
@@ -3,7 +3,7 @@ console.log('Environment variables loaded from:', __dirname + '/.env');
 
 // Force set MONGODB_URI if not set in .env
 if (!process.env.MONGODB_URI) {
-  process.env.MONGODB_URI = 'mongodb+srv://irfansamjath007_db_user:v9QkyRmmJ0TcDcEa@traveldiaries.wdjllmv.mongodb.net/?retryWrites=true&w=majority&appName=Traveldiaries';
+  process.env.MONGODB_URI = 'mongodb+srv://irfansamjath007_db_user:F1VTUDGEuAKNTpzJ@traveldiaries.wdjllmv.mongodb.net/?retryWrites=true&w=majority&appName=Traveldiaries';
   console.log('MONGODB_URI was not set, defaulting to:', process.env.MONGODB_URI);
 }
 
